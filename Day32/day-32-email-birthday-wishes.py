39b5bcdf8feb68b5dc29b45aa43b0bcc5e602a80
Christopher Hagan <christopherhagan1985@gmail.com>
2021-06-14 22:25:14 +0100

    Fixed a bug where I hadn't specified user email

--- Day32/day-32-email-birthday-wishes.py
+++ Day32/day-32-email-birthday-wishes.py
@@ -20,8 +20,6 @@ def getArguments():
                     help="Email address")
     ap.add_argument("-p", "--password", required=False,
                     help="Password for user")
-    ap.add_argument("-r", "--recipient", required=False,
-                    help="Recipient email address")
     args = vars(ap.parse_args())
     return args
 
@@ -34,28 +32,25 @@ if arguments['user'] is None:
 if arguments['password'] is None:
     arguments['password'] = getpass.getpass('Enter your Password: ')
 
-if arguments['recipient'] is None:
-    arguments['recipient'] = input('Enter the recipient\'s email: ')
-
 birthday_data = pandas.read_csv('birthdays.csv')
 
 today = dt.datetime.now()
 
-email_list = birthday_data[(birthday_data['month'] == today.month) & (birthday_data['day'] == today.day)]
+email_list_df = birthday_data[(birthday_data['month'] == today.month) & (birthday_data['day'] == today.day)]
 
-if not email_list.empty:
-    for index in email_list.index:
+if not email_list_df.empty:
+    for index in email_list_df.index:
         letter = random.choice(['letter_1.txt', 'letter_2.txt', 'letter_3.txt'])
         with open('letter_templates/{}'.format(letter)) as letter_contents:
             email_body = letter_contents.read()
 
-        email_body = email_body.replace('[NAME]', email_list['name'][index])
+        email_body = email_body.replace('[NAME]', email_list_df['name'][index])
 
         with smtplib.SMTP('smtp.gmail.com') as connection:
             connection.starttls()
             connection.login(user=arguments['user'], password=arguments['password'])
             connection.sendmail(
                 from_addr=arguments['user'], 
-                to_addrs=arguments['recipient'], 
+                to_addrs=email_list_df['email'][index], 
                 msg='Subject:Happy Birthday!\n\n{}'.format(email_body)
             )
